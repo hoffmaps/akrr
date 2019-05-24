7d613e95ed6ca27166352919c7c57a915bbf40ac
hoffmaps <hoffmaps@lemoyne.edu>
2019-05-24 15:28:21 -0400

    updated files, working through making the bash script

--- docker/hpcc_intelmpi/execs/hpcc-1.5.0/set_input_file.sh
+++ docker/hpcc_intelmpi/execs/hpcc-1.5.0/set_input_file.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-echo "hello test"
+# input file is argument 1 in this case if it's right after docker argument
+HPCCINF=$1 
 
+INFILE="default_hpccinf.txt"
 
-echo '$0 = ' $0
-echo '$1 = ' $1
+echo "You input the following file: " $HPCCINF
+
+# checks if the given file is not the default file - need to change file name in this case
+if [ $HPCCINF != "default" ]
+then
+	INFILE=$HPCCINF
+fi
+
+# goes to the input directory to get file
+cd ./inputs
+
+if [ -r $INFILE ]
+then
+	cp $INFILE ../hpccinf.txt
+else
+	echo "File does not exist in inputs or does not have read permissions"
+fi
+
+cd ..
+
+echo "The file $INFILE is now hpccinf.txt in the same directory as hpcc"
+#echo '$0 = ' $0
+#echo '$1 = ' $1
+
+pwd
 
 # this allows us to go back into bash (nice for debugging and such)
-/bin/bash
+#/bin/bash
